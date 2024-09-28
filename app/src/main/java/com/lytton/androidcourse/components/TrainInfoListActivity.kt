a35a2647012f762e8107c0d9ae30cad643c9c1e5
Lytton Yang <azure12355@gmail.com>
2024-09-28 11:20:23 +0800

    feat: 新增车次信息项布局文件

---
+++ app/src/main/java/com/lytton/androidcourse/components/TrainInfoListActivity.kt
@@ -0,0 +1,21 @@
+package com.lytton.androidcourse.components
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.lytton.androidcourse.R
+
+class TrainInfoListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_train_info_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
