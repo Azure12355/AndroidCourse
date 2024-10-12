fc58a8b0239dc98a9fe708881a99cb9c5246135b
Lytton Yang <azure12355@gmail.com>
2024-10-12 10:34:46 +0800

    feat: 完成车票查询布局文件

--- app/src/main/java/com/lytton/androidcourse/components/TicketInquiryActivity.kt
+++ app/src/main/java/com/lytton/androidcourse/components/TicketInquiryActivity.kt
@@ -1,21 +1,8 @@
 package com.lytton.androidcourse.components
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.lytton.androidcourse.R
+import com.lytton.androidcourse.base.activity.BaseViewModelActivity
+import com.lytton.androidcourse.databinding.ActivityTicketInquiryBinding
 
-class TicketInquiryActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_ticket_inquiry)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+class TicketInquiryActivity : BaseViewModelActivity<ActivityTicketInquiryBinding>() {
+    
 }
