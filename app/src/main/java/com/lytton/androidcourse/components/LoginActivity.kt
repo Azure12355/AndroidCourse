9fa4df902cc611c0f8a2d200f3f521f966037f02
Lytton Yang <azure12355@gmail.com>
2024-10-13 10:32:25 +0800

    feat: 创建登录界面布局文件和窗口

---
+++ app/src/main/java/com/lytton/androidcourse/components/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.lytton.androidcourse.components
+
+import com.lytton.androidcourse.base.activity.BaseViewModelActivity
+import com.lytton.androidcourse.databinding.ActivityLoginBinding
+
+/**
+ * @program: AndroidCourse
+ * @description:
+ * @author: LyttonYang
+ * @create: 2024-10-13 10:30
+ */
+class LoginActivity : BaseViewModelActivity<ActivityLoginBinding>() {
+}
